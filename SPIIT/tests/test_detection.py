22f5d90ba74d32aaa2045dcfbaf8bf8266a1c7bf
Zachary Samuels <capnsurvivalist@gmail.com>
2023-06-01 11:52:50 -0600

    Connection Detection Tests to Detection Code

---
+++ SPIIT/tests/test_detection.py
@@ -0,0 +1,73 @@
+import unittest
+import sys
+sys.path.insert(1, 'C:\\Users\\zacha\\Documents\\Python\\GPT4 LangChain PII Tokenization\\Project-2-Secure-PII-Tokenization-for-LLMs\\SPIIT')
+from main import remove
+
+def scrubPII(input):
+    return remove(input)
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_phonenumbers(self):
+        self.assertEqual(scrubPII("Hello, my phone number is 713-392-8748"), "Hello, my phone number is xxxxxxxxxx")
+        self.assertEqual(scrubPII("Hello, my phone number is (713)-392-8748"), "Hello, my phone number is xxxxxxxxxxxx")
+        self.assertEqual(scrubPII("Hello, my phone number is 7133928748"), "Hello, my phone number is xxxxxxxxxx")
+
+    def test_names(self):
+        self.assertEqual(scrubPII("Hello, my name is John Smith"), "Hello, my name is xxxx xxxxx")
+        self.assertEqual(scrubPII("Ethan Williams is a great guy!"), "xxxxx xxxxxxxx is a great guy!")
+        self.assertEqual(scrubPII("Jeff Bezos owns Amazon"), "xxxx xxxxx owns Amazon")
+
+    def test_address(self):
+        self.assertEqual(scrubPII("I live at 1234 Evergreen St."), "I live at xxxx xxxxxxxxx xx")
+        self.assertEqual(scrubPII("I think 4235 Overpass Rd. is the address for that store"), "I think xxxx xxxxxxxx xx is the address for that store")
+        self.assertEqual(scrubPII("I'm heading to 5 Waverly Court for the party."), "I'm heading to x xxxxxxx xxxxx for the party.")
+
+    def test_ssn(self):
+        self.assertEqual(scrubPII("My SSN is 134-14-1513"), "My SSN is xxxxxxxxx")
+        self.assertEqual(scrubPII("421-74-2672 is a SSN"), "xxxxxxxxx is a SSN")
+        self.assertEqual(scrubPII("I love sharing social security numbers such as 512-35-1561"), "I love sharing social security numbers such as xxxxxxxxx")
+
+    def test_email(self):
+        self.assertEqual(scrubPII("You can reach me at me.person@gmail.com"), "You can reach me at xxxxxxxxxxxxxxxxxxx")
+        self.assertEqual(scrubPII("joe@mama.com is a really funny email"), "xxxxxxxxxxxx is a really funny email")
+        self.assertEqual(scrubPII("johnsmith@hotmail.com is where you can reach him"), "xxxxxxxxxxxxxxxxxxxxx is where you can reach him")
+
+    def test_dates(self):
+        self.assertEqual(scrubPII("I was born on May 2nd, 1992"), "I was born on xxx xxx, xxxx")
+        self.assertEqual(scrubPII("02/25/1956 was a long time ago"), "xxxxxxxxxx was a long time ago")
+        self.assertEqual(scrubPII("When were you born? I was born on 4/12/2002"), "When were you born? I was born on xxxxxxxxx")
+
+    def test_ip(self):
+        self.assertEqual(scrubPII("My private IP is 192.168.1.1"), "My private IP is xxxxxxxxxxx")
+        self.assertEqual(scrubPII("My public IP is 138.66.198.94"), "My public IP is xxxxxxxxxxxxx")
+        self.assertEqual(scrubPII("Another example of a public IP is 17.5.7.3"), "Another example of a public IP is xxxxxxxx")
+
+    def test_creditcards(self):
+        self.assertEqual(scrubPII("My mom's credit card number is 4352 7200 5136 2812"), "My mom's credit card number is xxxx xxxx xxxx xxxx")
+        self.assertEqual(scrubPII("My credit card number is 4352720062352512"), "My credit card number is xxxxxxxxxxxxxxxx")
+        self.assertEqual(scrubPII("My dad's credit card number is 4352-7200-5624-2562"), "My mom's credit card number is xxxx-xxxx-xxxx-xxxx")
+
+    def test_driverslicense(self):
+        self.assertEqual(scrubPII("Apparently it says my number is DL-125566235"), "Apparently it says my number is xxxxxxxxxxxx")
+        self.assertEqual(scrubPII("His driver's license number is 56123594"), "His driver's license number is xxxxxxxx")
+        self.assertEqual(scrubPII("I am able to and my DL# is 1251553"), "I am able to and my DL# is xxxxxxx")
+
+    def test_passport(self):
+        self.assertEqual(scrubPII("His passport number is A43240234"), "His passport number is xxxxxxxxx")
+        self.assertEqual(scrubPII("I can use my passport to get in which has 541022134 as its number"), "I can use my passport to get in which has xxxxxxxxx as its number")
+        self.assertEqual(scrubPII("E000145028 is a cool passport number honestly."), "xxxxxxxxx is a cool passport number honestly.")
+    
+    def test_idnumber(self):
+        self.assertEqual(scrubPII("His state id is 41366234"), "His state id is xxxxxxxx")
+        self.assertEqual(scrubPII("He doesn't have a citizenship so his id is 25129491"), "He doesn't have a citizenship so his id is xxxxxxxx")
+        self.assertEqual(scrubPII("87172412 is an id number"), "xxxxxxxx is an id number")
+
+    def test_bankaccount(self):
+        self.assertEqual(scrubPII("My bank account number is 132513513231 at Bank of America"), "My bank account number is xxxxxxxxxxxx at xxxx xx xxxxxxx")
+        self.assertEqual(scrubPII("You can use the routing number 192384592189"), "You can use the routing number xxxxxxxxxxxx")
+        self.assertEqual(scrubPII("Use the banking number 928191841"), "Use the banking number xxxxxxxxx")
+
+
+if __name__ == '__main__':
+    unittest.main()
