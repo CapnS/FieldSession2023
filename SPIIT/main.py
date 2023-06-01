07132965dc1542c19974ea6ffc566e2bb7b78153
Brendan <bmoroney@mines.edu>
2023-06-01 09:32:51 -0600

    Updated main with list linking PII and token, added function to send token data to snowflake.

--- SPIIT/main.py
+++ SPIIT/main.py
@@ -10,6 +10,7 @@ from transformers import pipeline
 import snowflake.connector
 from dotenv import load_dotenv
 import os
+import datetime
 
 # N - Name 
 # A - Address 
@@ -36,32 +37,35 @@ def remove(text):
     email_validate_pattern = r"^\S+@\S+\.\S+$"
     ip_validate_pattern = "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
     mac_validate_pattern = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})|([0-9a-fA-F]{4}\\.[0-9a-fA-F]{4}\\.[0-9a-fA-F]{4})$"
+    passport_pattern_old = r"^[A-Z0-9]{9}$"
+    passport_pattern_new = r"^[A-Z]\d{8}$"
     # we can add more cc types
     amex_validate_pattern = "^3[47][0-9]{13}$"
     visa_validate_pattern = "^4[0-9]{12}(?:[0-9]{3})?$"
     master_card_validate_pattern = "^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$"
 
     #read pdf and store it as a string 
-    # TODO: REMOVE THIS AND JUST USE FUNCTION INPUT
-    p = open('1 Email, 1 MAC Address.pdf', 'rb')
+    p = open('SPIIT/Invoice  with lots of Passport Numbers, names, ssns, and drivers license numbers.pdf', 'rb')
     pdf = PyPDF2.PdfReader(p)
     text = pdf.pages[0].extract_text() + "\n"
     text = " ".join(text.replace(u"\xa0", " ").strip().split())
     text2 = text 
-    
+    text4 = text 
 
     #load text in nlp
     ner = pipeline("ner", model=ner_model, grouped_entities=True)
     output = ner(text)
 
-    print("Original string: ")
-    print(text)
+    #print(output)
+
+    #print("Original string: ")
+    #print(text)
     text3 = text
 
     #key words lists
     undetected_pii_list = ["DL", "driver's license", "driving permit", "driver license", "drivers license", "dl#","dls#", "lic#","lics#", "ID", "passport", "bank account", "account number", "Passport number"]
     passport_words = ["passport number" , "passport#"]
-    dl_words = ["DL", "driver's license", "driving permit", "driver license", "drivers license", "dl#","dls#", "lic#","lics#"]
+    dl_words = ["DL", "driver's license", "driving permit", "driver license", "drivers license", "dl#","dls#", "lic#","lics#", "licenses"]
     id_words = ["ID", "ID#", "ID number", "identification documents", "ID card"]
 
     #empty lists will be used to collect all the data associated with it and further stored in one nested loop to keep all the pii
@@ -77,11 +81,75 @@ def remove(text):
     org_list = []
     ip_list = []
     cc_list = []
+    passport_list = []
+    all_pii = []
+
+    #parts of the addresses sometimes wrongfully identified as names, so addresses needs to extracted first to avoid that 
+    for entity_group in output:
+        entity_label = entity_group["entity_group"]  
+        if entity_label == "LOC":
+            temp = entity_group["word"]
+            start_ind = text.find(temp)
+            end_ind = start_ind + len(temp)
+            if not text[end_ind].isspace() and text[end_ind] not in punc_list:
+                end_ind = text.find(" ", end_ind)
+                temp = text[start_ind:end_ind]
+            
+            #print("Here is a substring ")
+            # Find the previous word and check if it is numeric to identify if it is part of the address 
+            previous_word = text.rfind(" ", 0, start_ind-1)
+            #print(previous_word)
+            # Extract the previous word using string slicing
+            p_word = text[previous_word + 1:start_ind-1]
+            isDigit = True;
+            for x in p_word:
+                if x.isdigit() == False:
+                    isDigit == False;
+            # if it is numeric combine with the rest of address 
+            if isDigit and len(p_word) < 5:
+                start_ind = previous_word
+                temp = p_word + " " + temp 
+            address_list.append(temp)
+            #mask it with xxx for testing purposes 
+            x = 'x';
+            for n in range (len(temp)-1):
+                x = x + 'x';             
+            text = text[:start_ind] + x + text[start_ind+len(x):]
+    #print(text)
+
+    # extract punctuation for better performance of the libraries 
+    for i in text4:
+        if i in punc_list:
+            if i == '.':
+                text4 = text4.replace(i, " ")
+            else:
+                text4 = text4.replace(i, "")
+
+    # detect passport number and replace it with xxx
+    # only new passports have a format that unique from other 
+    for word in text4.split():
+        #edge case: make sure that there is no punctuation after the passport number; otherwise, it will not be detected
+        if word[-1] in punc_list:
+            word = word[:-1]
+        if (re.match(passport_pattern_new, word)):
+
+            passport_list.append(word)
+            x = 'x';
+            for n in range (len(word)-1):
+                x = x + 'x'; 
+            text = text.replace(word, x)
+            
+    new_list = []
+    new_list.append("R")
+    new_list.append(passport_list)
+    all_pii.append(new_list)
+
 
     # load data from nlp into lists associated with it 
     for entity_group in output:
         
-        entity_label = entity_group["entity_group"]    
+        entity_label = entity_group["entity_group"]  
+
         if entity_label == "PER":
             # check if the word is complete since the model sometimes tempts to grab just the beginning of the name 
             temp = entity_group["word"]
@@ -97,18 +165,29 @@ def remove(text):
                 x = x + 'x';             
             text = text[:start_ind] + x + text[start_ind+len(x):]
         elif entity_label == "MISC":
-            undefined.append(entity_group["word"])
+            # check if the word is complete since the model sometimes tempts to grab just the beginning of the name 
+            temp = entity_group["word"]
+            start_ind = text.find(temp)
+            end_ind = start_ind + len(temp)
+            if not text[end_ind].isspace() and text[end_ind] not in punc_list:
+                end_ind = text.find(" ", end_ind)
+                temp = text[start_ind:end_ind]
+            undefined.append(temp)
+            #mask it with xxx for testing purposes 
+            x = 'x';
+            for n in range (len(temp)-1):
+                x = x + 'x';             
+            text = text[:start_ind] + x + text[start_ind+len(x):]
         elif entity_label == "ORG":
             org_list.append(entity_group["word"])
-        elif entity_label == "LOC":
-            address_list.append(entity_group["word"])
+
             
 
     # create temporary new lists that will hold the char associated with pii and a list of all piis 
     # then append each list in one nested list all_pii 
     # so we end up getting something like this:
     # all_pii = [['N', ['Chris Johnson']], ['O', ['Diamond Star International', 'Diamond Star']], ['P', ['713-832-1234']]]
-    all_pii = []
+
     new_list = []
     new_list.append("N")
     new_list.append(people_list)
@@ -168,28 +247,35 @@ def remove(text):
     clean_undetected_list = [] 
 
     for sentence in new_list:
+        #print(sentence)
         sentence_list = []
+        #check if there is only one type of the document mentioned in the sentence 
+        #if there id more than one, the pii will go into undefined list
+        one_type = 0;
         # assign char depending on the type of pii
-        for word in passport_words:
-            if word.lower() in sentence.lower():
-                clean_undetected_list.append("R")
         for word in id_words:
             if word.lower() in sentence.lower():
-                clean_undetected_list.append("B")
+                one_type = one_type + 1
+                char = "B"
         for word in dl_words:
             if word.lower() in sentence.lower():
-                clean_undetected_list.append("D")
-        #trace the the digital pii 
+                one_type = one_type + 1
+                char = "D"
+        #trace the the numeric pii 
         for word in sentence.split():
             isDigit = False;
             for char in word:
                 if char.isdigit() == True:
                     isDigit = True; 
-            if isDigit == True:
+            if isDigit == True and len(word) > 5 and word[0] != "$":
                 # sometimes it will grab pii with punctuation, so we need to make sure to get rid of it before passing into the list 
                 if word[-1] in punc_list:
                     word = word[:-1]
-                sentence_list.append(word)
+                if one_type == 1:
+                    clean_undetected_list.append(char)
+                    sentence_list.append(word)
+                else:
+                    undefined.append(word)
                 #replace pii with xxx
                 if text.find(word) > -1:
                     index = text.find(word)
@@ -197,10 +283,10 @@ def remove(text):
                     for n in range (len(word)-1):
                         x = x + 'x';             
                     text = text[:index] + x + text[index+len(x):]
-
-        clean_undetected_list.append(sentence_list)
-    if len(clean_undetected_list) > 0:
-        all_pii.append(clean_undetected_list)
+        if len(sentence_list) > 0 :
+            clean_undetected_list.append(sentence_list)
+    #if len(clean_undetected_list) > 0:
+        #all_pii.append(clean_undetected_list)
 
     # detect ssn and replace it with xxx
     for word in text.split():
@@ -294,7 +380,7 @@ def remove(text):
     all_pii.append(new_list)
 
     #second nlp that we are using for dates 
-    nlp = spacy.load("en_core_web_sm")
+    nlp = spacy.load('en_core_web_sm')
     doc = nlp(text)            
     
     # using spacy library to detect dates
@@ -313,14 +399,12 @@ def remove(text):
                     for n in range (len(word)-1):
                         x = x + 'x';             
                     text = text[:index] + x + text[index+len(x):]
-
-    # TODO: REMOVE THIS PRINT STATEMENT
-    print()
-    print("String with masked pii: ")
-    print(text)
+    #print()
+    #print("String with masked pii: ")
+    #print(text)
     new_list = []
     new_list.append("D")
-    new_list.append(date_list)
+    #new_list.append(date_list)
 
     # We have to figure out what to do with dates; we will probably just have to manually check if they match the format of the DOB because we don't 
     # need to tokenize all the dates 
@@ -328,19 +412,25 @@ def remove(text):
     #all_pii.append(new_list)
     #print(all_pii)
 
+    token_list = []
+
     #sub the pii with uuid to pass it to chat gpt
     for pii_list in all_pii:
         if len(pii_list[1]) != 0:
             char = pii_list[0][0]
             for element in pii_list[1]:
+                temp = []
+                temp.append(char)
+                temp.append(element)
                 index = text3.find(element)
                 str = uuid.uuid4() 
+                temp.append(str.hex)
                 text3 = text3[:index] + char + "-" + str.hex + text3[index+len(element):]
-    # TODO: MATCH PII WITH TOKEN
-    print()            
-    print("String with UUID pii: ")
-    print(text3)
-    
+                token_list.append(temp)
+                
+    #sends token data to snowflake
+        
+    send_to_snowflake(token_list)
 
     # TODO: Write to file
 
@@ -349,50 +439,54 @@ def replace(text):
     # write to file 
     pass
 
+#TODO: Write function to initialize snowflake connection and databases
 #Database 
+def send_to_snowflake(token_list):
+    ct = datetime.datetime.now()
+    load_dotenv()
+    SNOWFLAKE_PASSWORD = os.getenv("SNOWFLAKE_PASSWORD")
 
-load_dotenv()
-SNOWFLAKE_PASSWORD = os.getenv("SNOWFLAKE_PASSWORD")
+    con_def = snowflake.connector.connect(user='BRENDANMORONEY',
+                                        account='ydpcciy-xn91624',
+                                        password =SNOWFLAKE_PASSWORD,
+                                        database='PII_TOKENIZATION',        
+                                        schema ='PUBLIC',
+                                        autocommit=True)         
 
-con_def = snowflake.connector.connect(user='BRENDANMORONEY',
-                                     account='ydpcciy-xn91624',
-                                     password =SNOWFLAKE_PASSWORD,
-                                     database='PII_TOKENIZATION',        
-                                     schema ='PUBLIC',
-                                     autocommit=True)         
+    db_cursor_def = con_def.cursor()
 
-db_cursor_def = con_def.cursor()
+    db_cursor_def.execute("CREATE WAREHOUSE IF NOT EXISTS pii_warehouse")
+    db_cursor_def.execute("USE WAREHOUSE pii_warehouse")
 
-db_cursor_def.execute("CREATE WAREHOUSE IF NOT EXISTS pii_warehouse")
-db_cursor_def.execute("USE WAREHOUSE pii_warehouse")
+    db_cursor_def.execute("CREATE DATABASE IF NOT EXISTS PII_TOKENIZATION")
+    db_cursor_def.execute("USE DATABASE PII_TOKENIZATION")
 
-db_cursor_def.execute("CREATE DATABASE IF NOT EXISTS PII_TOKENIZATION")
-db_cursor_def.execute("USE DATABASE PII_TOKENIZATION")
+    db_cursor_def.execute("CREATE SCHEMA IF NOT EXISTS PUBLIC")
+    db_cursor_def.execute("USE SCHEMA PII_TOKENIZATION.PUBLIC")
 
-db_cursor_def.execute("CREATE SCHEMA IF NOT EXISTS PUBLIC")
-db_cursor_def.execute("USE SCHEMA PII_TOKENIZATION.PUBLIC")
+    #Creates table with PII_value, PII_type and ID
 
-#Creates table with PII_value, PII_type and ID
+    db_cursor_def.execute("""CREATE OR REPLACE TABLE 
+    PII_TOKENIZATION.PUBLIC.PII_Token_XREF (Token TEXT, PII_VALUE 
+    VARCHAR(16777216),PII_TYPE VARCHAR(16777216), rec_created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP(), 
+    user_added TEXT, updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP(), PRIMARY KEY(Token))""")
 
-db_cursor_def.execute("""CREATE OR REPLACE TABLE 
-PII_TOKENIZATION.PUBLIC.PII_Token_XREF (Token TEXT, PII_VALUE 
-VARCHAR(16777216),PII_TYPE VARCHAR(16777216), rec_created_date TIMESTAMP, 
-user_added TEXT, updated_date TIMESTAMP, PRIMARY KEY(Token))""")
+    #Creates log table
 
-#Creates log table
+    db_cursor_def.execute("""CREATE OR REPLACE TABLE PII_TOKENIZATION.PUBLIC.log 
+    (time TIMESTAMP, user TEXT, document TEXT, PII_type TEXT, override 
+    boolean)""")
+                        
+    #Practice insert
+    #Inserts data into table
+    #TODO add user_added
+    for token in token_list:
+        db_cursor_def.execute("INSERT INTO PII_Token_XREF(Token, PII_VALUE, PII_TYPE) VALUES(%s, %s, %s)", (token[2], token[1], token[0]))
 
-db_cursor_def.execute("""CREATE OR REPLACE TABLE PII_TOKENIZATION.PUBLIC.log 
-(time TIMESTAMP, user TEXT, document TEXT, PII_type TEXT, override 
-boolean)""")
-                      
-#Practice insert
-#TODO when PII is matched to Token, add functionality to store and remove from database
-
-db_cursor_def.execute("INSERT INTO PII_Token_XREF(Token, PII_VALUE, PII_TYPE) VALUES('c2783f59-743e-403c-beac-21cb67076292','Rick Owens', 'N')")
-
-db_cursor_def.close()
+    db_cursor_def.close()
 
 if __name__ == "__main__":
+    remove("wow")
     text = sys.argv()[1]
-    remove(text) if sys.argv()[2] == "1" else replace(text)
+    #remove(text) if sys.argv()[2] == "1" else replace(text)
 
