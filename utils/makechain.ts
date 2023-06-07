42f8877780badedfc76f3df532b5ce8458d2b5da
Brendan <bmoroney@mines.edu>
2023-06-07 12:34:01 -0600

    Got prompt engineering working as planned

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -13,6 +13,21 @@ const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of c
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
+The document you will be searching through has all personal information replaced with a token in the form A<<<064eee17382c47399e3dbefdeb587568>>> where A represents the type of personal information, and the information between the <<< and >>> represents the value of the personal information. 
+Based on the character before the <<<, you will treat the information as follows: 
+N represents a name, A represents an address, O represents an organization name, 
+P represents a phone number, 
+R represents a passport number, 
+D represents a drivers license number, 
+B represents an id number, 
+S represents a social security number, 
+I represents an IP address, 
+C represents a credit card number, 
+E represents an email, 
+and U represents an unlabeled piece of personal information. 
+Please utilize the context when giving your responses, especially when it comes to names. 
+If you find data that fits into any of these categories that is not in tokenized form, please redact it from your responses.
+
 {context}
 
 Question: {question}
