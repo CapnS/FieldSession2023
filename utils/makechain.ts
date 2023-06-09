add7a8f70b2c5b661584e58b082ba7bdda644206
Zach Samuels <capnsurvivalist@gmail.com>
2023-06-09 13:25:51 -0500

    Unfinished prompt engineering

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -13,7 +13,7 @@ const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of c
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
-The document you will be searching through has all personal information replaced with a token in the form A<<<064eee17382c47399e3dbefdeb587568>>> where A represents the type of personal information, and the information between the <<< and >>> represents the value of the personal information. 
+The context you will be searching through has all personal information replaced with a token in the form A<<<064eee17382c47399e3dbefdeb587568>>> where A represents the type of personal information, and the information between the <<< and >>> represents the value of the personal information. 
 Based on the character before the <<<, you will treat the information as follows: 
 N represents a name, 
 A represents an address, 
@@ -28,9 +28,9 @@ C represents a credit card number,
 E represents an email, 
 L represents a date,
 and U represents an unlabeled piece of personal information. 
-Please utilize the context when giving your responses, especially when it comes to names. Some people might have different tokens for their full name and just their last name, so make sure to treat those two tokens as the same person if you encounter a situation like that.
-Never respond with just the character, always give the full token in the correct form. 
-If you find data that fits into any of these categories that is not in tokenized form, please redact it from your responses.
+Please utilize the context when giving your responses, especially when it comes to names. 
+For example, some people might have different tokens correlated to them. One for their first name and one for their full name, so make sure to use context clues to look out for this and treat those two tokens as the same person.
+Make sure you never respond with just the character in place of one of these values, always give the full token in the correct form that I gave you above. 
 
 {context}
 
